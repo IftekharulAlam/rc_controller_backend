449e5f27f3e188eb21aceb7f8dbf64562f42e5fe
Iftekharul Alam <iftekharulalam38@gmail.com>
2022-11-03 13:57:45 +0600

    enable disable code added

--- rc_controller_backend/urls.py
+++ rc_controller_backend/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
   path('getData', views.getData, name='getData'),
+  path('writeData', views.writeData, name='writeData'),
+  path('writeStatus', views.writeStatus, name='writeStatus'),
+  path('updateStatus', views.updateStatus, name='updateStatus'),
 
 ]
--- rc_controller_backend/views.py
+++ rc_controller_backend/views.py
@@ -11,20 +11,49 @@ def getData(request):
     if request.method == 'GET':
         status = "Enable"
         with connection.cursor() as cursor_1:
-            cursor_1.execute("SELECT Command FROM car1 inner join carstatus on car1.DeviceId = carstatus.DeviceId where carstatus.status = '"+str(status) + "' ORDER BY Serial DESC LIMIT 1")
+            cursor_1.execute("SELECT Command FROM car1 inner join carstatus on car1.DeviceId = carstatus.DeviceId where carstatus.status = '" +
+                             str(status) + "' ORDER BY Serial DESC LIMIT 1")
             row1 = cursor_1.fetchone()
-            response_data = {}
-            response_data['result'] = row1[0]
-            json_data = json.dumps(response_data)
-            return HttpResponse(json_data, content_type="application/json")
+            if row1 == None:
+                return HttpResponse("", content_type="application/json")
+            else:
+                response_data = {}
+                response_data['result'] = row1[0]
+                json_data = json.dumps(response_data)
+                return HttpResponse(json_data, content_type="application/json")
+
 
 @csrf_exempt
 def writeData(request):
     if request.method == 'POST':
+        deviceID = request.POST.get("deviceID", False)
         command = request.POST.get("command", False)
-        deviceID =  request.POST.get("deviceID", False)
         with connection.cursor() as cursor_1:
-            cursor_1.execute("INSERT INTO car1(DeviceId,Command) VALUES ('"+int(deviceID) + "','"+str(command) + "')")
+            cursor_1.execute("INSERT INTO car1(DeviceId,Command) VALUES ('"+str(deviceID) + "','"+str(command) + "')")
+            connection.commit()
+          
+
+        return HttpResponse("", content_type="application/json")
+
+@csrf_exempt
+def writeStatus(request):
+    if request.method == 'POST':
+        deviceID = request.POST.get("deviceID", False)
+        status = request.POST.get("status", False)
+        with connection.cursor() as cursor_1:
+            cursor_1.execute("INSERT INTO carstatus(DeviceId,status) VALUES ('"+str(deviceID) + "','"+str(status) + "')")
+            connection.commit()
+          
+
+        return HttpResponse("", content_type="application/json")
+
+@csrf_exempt
+def updateStatus(request):
+    if request.method == 'POST':
+        deviceID = request.POST.get("deviceID", False)
+        status = request.POST.get("status", False)
+        with connection.cursor() as cursor_1:
+            cursor_1.execute("update carstatus set status='"+str(status) + "' where DeviceId = '"+str(deviceID) + "'")
             connection.commit()
           
 
