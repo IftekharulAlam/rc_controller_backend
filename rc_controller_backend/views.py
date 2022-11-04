f1755af409c12703f08a4b4620c290636b918410
Iftekharul Alam <iftekharulalam38@gmail.com>
2022-11-04 22:26:21 +0600

    commit

--- rc_controller_backend/views.py
+++ rc_controller_backend/views.py
@@ -7,6 +7,15 @@ from django.http import FileResponse, HttpResponse, JsonResponse
 
 # Create your views here.
 @csrf_exempt
+def createTable(request):
+    with connection.cursor() as cursor_1:
+        cursor_1.execute("")
+        connection.commit()
+
+        return HttpResponse("", content_type="application/json")
+
+
+@csrf_exempt
 def getData(request):
     if request.method == 'GET':
         status = "Enable"
@@ -14,6 +23,8 @@ def getData(request):
             cursor_1.execute("SELECT Command FROM car1 inner join carstatus on car1.DeviceId = carstatus.DeviceId where carstatus.status = '" +
                              str(status) + "' ORDER BY Serial DESC LIMIT 1")
             row1 = cursor_1.fetchone()
+            connection.close()
+        
             if row1 == None:
                 return HttpResponse("", content_type="application/json")
             else:
@@ -23,17 +34,20 @@ def getData(request):
                 return HttpResponse(json_data, content_type="application/json")
 
 
+
 @csrf_exempt
 def writeData(request):
     if request.method == 'POST':
         deviceID = request.POST.get("deviceID", False)
         command = request.POST.get("command", False)
         with connection.cursor() as cursor_1:
-            cursor_1.execute("INSERT INTO car1(DeviceId,Command) VALUES ('"+str(deviceID) + "','"+str(command) + "')")
+            cursor_1.execute("INSERT INTO car1(DeviceId,Command) VALUES ('" +
+                             str(deviceID) + "','"+str(command) + "')")
             connection.commit()
-          
+            connection.close()
 
         return HttpResponse("", content_type="application/json")
+
 
 @csrf_exempt
 def writeStatus(request):
@@ -41,11 +55,13 @@ def writeStatus(request):
         deviceID = request.POST.get("deviceID", False)
         status = request.POST.get("status", False)
         with connection.cursor() as cursor_1:
-            cursor_1.execute("INSERT INTO carstatus(DeviceId,status) VALUES ('"+str(deviceID) + "','"+str(status) + "')")
+            cursor_1.execute(
+                "INSERT INTO carstatus(DeviceId,status) VALUES ('"+str(deviceID) + "','"+str(status) + "')")
             connection.commit()
-          
+            connection.close()
 
         return HttpResponse("", content_type="application/json")
+
 
 @csrf_exempt
 def updateStatus(request):
@@ -53,8 +69,9 @@ def updateStatus(request):
         deviceID = request.POST.get("deviceID", False)
         status = request.POST.get("status", False)
         with connection.cursor() as cursor_1:
-            cursor_1.execute("update carstatus set status='"+str(status) + "' where DeviceId = '"+str(deviceID) + "'")
+            cursor_1.execute("update carstatus set status='" +
+                             str(status) + "' where DeviceId = '"+str(deviceID) + "'")
             connection.commit()
-          
+            connection.close()
 
         return HttpResponse("", content_type="application/json")
